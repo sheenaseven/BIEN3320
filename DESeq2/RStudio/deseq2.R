cb131548cbabb8018ef27e51e7135538b2f89c34
sheena7 <sheena7@egsu101.ust.hk>
2021-02-15 22:18:47 +0800

    update

---
+++ DESeq2/RStudio/deseq2.R
@@ -0,0 +1,48 @@
+# if (!requireNamespace("BiocManager", quietly = TRUE))
+#   install.packages("BiocManager")
+# BiocManager::install(version = "3.12")
+# BiocManager::install("DESeq2")
+
+library("DESeq2")
+#1 read data
+setwd("~/Dropbox/course/BIEN3320/Tutorial/T03/")
+countMatrix <- as.matrix(read.csv("count.csv",sep=",",row.names="gene"))
+info <- read.csv("info.csv", row.names=1)
+info$condition <- factor(info$condition)
+
+# check the sample order
+all(rownames(info) == colnames(countMatrix))
+
+###2 DESeq2
+dds <- DESeqDataSetFromMatrix(countData = countMatrix,
+                              colData = info,
+                              design = ~ condition)
+dds
+
+#3 pre-filtering
+dds <- dds[rowSums(counts(dds)) > 0,]
+
+#4 factor levels
+dds$condition <- factor(dds$condition, levels = c("control","tumor"))
+
+#5 Differential expression analysis
+dds <- DESeq(dds)
+results(dds)
+comparison=resultsNames(dds)[2]
+res <- results(dds, name=comparison)  #condition_tumor_vs_control; log2(tumor/control).  
+print(comparison)
+test=as.data.frame(res)
+##6 save to csv file
+res<-as.data.frame(res[complete.cases(res),])
+write.csv(res,'./deseq2.csv')
+
+counts(dds, normalized=TRUE)
+sizeFactors(dds)
+
+#plot the 
+par(mfrow=c(1,1))
+# Make a basic volcano plot
+with(res, plot(log2FoldChange, -log10(pvalue), pch=20, main="Volcano plot"))
+# Add colored points: blue if padj<0.05 & log2FoldChange<0, red if pvalue<.05 & log2FoldChange>0)
+with(subset(res, pvalue<.05 & log2FoldChange<0), points(log2FoldChange, -log10(pvalue), pch=20, col="blue"))
+with(subset(res, pvalue<.05 & log2FoldChange>0), points(log2FoldChange, -log10(pvalue), pch=20, col="red"))
